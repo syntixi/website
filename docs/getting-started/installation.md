d76f75934b213f9878b4ae2c82c1dd4dc6912a9f
Ta-Ching Chen <tachingchen@srcmesh.com>
2021-07-17 22:39:49 +0800

    first commit

---
+++ docs/getting-started/installation.md
@@ -0,0 +1,61 @@
+---
+sidebar_label: 'Installation'
+sidebar_position: 1
+---
+
+## Requirements
+
+* [Kubernetes](https://kubernetes.io/): A workable Kubernetes cluster is required before installing Syntixi. Following are some popular solutions to set up Kubernetes cluster with minimum effort.
+    * Docker Desktop: https://www.docker.com/products/docker-desktop
+    * Minikube: https://minikube.sigs.k8s.io/docs/start/
+* [Kubectl](https://kubernetes.io/docs/tasks/tools/): The version of Kubectl(Kubernetes CLI) should matches Kubernetes version you test against with.
+* [Helm](https://helm.sh/) >= 3.0
+
+## Installation
+
+### Add Helm Repository
+
+Add Syntixi helm repository.
+
+```bash
+$ helm repo add syntixi-charts https://charts.syntixi.dev/
+$ helm search repo syntixi-charts
+```
+
+### Create namespace
+
+Let's install Syntixi to namespace `syntixi`.
+
+```bash
+$ kubectl create namespace syntixi
+$ helm install --namespace syntixi --name-template syntixi syntixi-charts/syntixi:3.0
+```
+
+### Install CLI
+
+```bash
+$ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+```
+
+### Hello World!
+
+```bash
+# Add the stock NodeJS env to your Syntixi deployment
+$ syntixi env create --name nodejs --image syntixi/node-env
+
+# A javascript one-liner that prints "hello world"
+$ curl https://raw.githubusercontent.com/syntixi/examples/master/environments/nodejs/hello.js > hello.js
+
+# Upload your function code to syntixi
+$ syntixi bundle create --name hello-bundle --env nodejs --code hello.js
+
+# Create the function with the package just created
+$ syntixi function create --name hello --env nodejs --bundle hello-bundle 
+
+# Map GET /hello to your new function
+$ syntixi route create --method GET --url /hello --function hello
+
+# Run the function.  This takes about 100msec the first time.
+$ syntixi function test --name hello
+Hello, world!
+```
---
+++ docusaurus.config.js
@@ -0,0 +1,115 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Syntixi',
+  tagline: 'Serverless Framework on Kubernetes',
+  url: 'https://docs.syntixi.dev',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'syntixi', // Usually your GitHub org/user name.
+  projectName: 'syntixi', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Syntixi',
+      logo: {
+        alt: 'Syntixi Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Documentation',
+        },
+        // {to: '/blog', label: 'Blog', position: 'left'},
+        // {
+        //   type: 'docsVersionDropdown',
+        //   position: 'left',
+        // },
+        {
+          href: 'https://github.com/syntixi/syntixi',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/syntixi/syntixi',
+            },
+            {
+              label: 'About Us',
+              href: 'https://srcmesh.com/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Srcmesh Co., Ltd.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/syntixi/documentation/edit/master/docs/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
